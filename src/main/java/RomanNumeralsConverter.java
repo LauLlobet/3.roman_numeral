59529c06a1772738d6069d0a413c3a3b6fe16ba9
Lau Llobet <lau.llobet@gmail.com>
2018-03-22 14:39:17 +0100

    refactored to  generic array to container

--- src/main/java/RomanNumeralsConverter.java
+++ src/main/java/RomanNumeralsConverter.java
@@ -1,38 +1,34 @@
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+
+import static java.util.Map.entry;
 
 public class RomanNumeralsConverter {
 
     public String convert(int arabic) {
-        Map<Integer,String> literals = new HashMap<Integer,String>();
-        literals.put(50,"L");
+        Map<Integer,String> literals = new LinkedHashMap();
+        literals.put(100,"C");
+        literals.put(50, "L");
         literals.put(10,"X");
-        literals.put(5,"V");
+        literals.put(5, "V");
         literals.put(1,"I");
-
         String roman = "";
+
         if(arabic == 4){
             return "IV";
         }
 
-        int literalArabic = 50;
-        if(arabic >= literalArabic){
-            roman = roman + literals.get(literalArabic);
-            arabic = arabic - literalArabic;
+
+        for (Map.Entry<Integer, String> literalArabicToRoman : literals.entrySet())
+        {
+            System.out.println(literalArabicToRoman.getKey());
+            int literalNumberKey = literalArabicToRoman.getKey();
+            while (arabic >= literalNumberKey){
+                roman = roman + literalArabicToRoman.getValue();
+                arabic = arabic - literalNumberKey;
+            }
         }
-        literalArabic = 10;
-        while(arabic >= 10){
-            roman = roman + literals.get(literalArabic);
-            arabic = arabic - literalArabic;
-        }
-        if(arabic >= 5){
-            roman =  "V";
-            arabic = arabic - 5;
-        }
-        while(arabic >= 1 ){
-            roman+= "I";
-            arabic = arabic - 1;
-        }
-        return roman ;
+
+        return roman;
     }
 }
