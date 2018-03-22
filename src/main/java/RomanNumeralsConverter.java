5f19cb96ae8bf0ee661b9b9391b4aa7a7c870503
Lau Llobet <lau.llobet@gmail.com>
2018-03-22 12:22:51 +0100

    ready to refactor

--- src/main/java/RomanNumeralsConverter.java
+++ src/main/java/RomanNumeralsConverter.java
@@ -1,19 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanNumeralsConverter {
 
     public String convert(int arabic) {
+        Map<Integer,String> literals = new HashMap<Integer,String>();
+        literals.put(50,"L");
+        literals.put(10,"X");
+        literals.put(5,"V");
+        literals.put(1,"I");
+
         String roman = "";
         if(arabic == 4){
             return "IV";
         }
-        if(arabic == 10){
-            return "X";
+
+        int literalArabic = 50;
+        if(arabic >= literalArabic){
+            roman = roman + literals.get(literalArabic);
+            arabic = arabic - literalArabic;
+        }
+        literalArabic = 10;
+        while(arabic >= 10){
+            roman = roman + literals.get(literalArabic);
+            arabic = arabic - literalArabic;
         }
         if(arabic >= 5){
             roman =  "V";
             arabic = arabic - 5;
         }
-        for(int i=0;i<arabic;i++){
+        while(arabic >= 1 ){
             roman+= "I";
+            arabic = arabic - 1;
         }
         return roman ;
     }
--- src/test/java/RomanNumeralsShould.java
+++ src/test/java/RomanNumeralsShould.java
@@ -17,7 +17,11 @@ public class RomanNumeralsShould {
             "5, V",
             "6, VI",
             "7, VII",
-            "10, X"
+            "10, X",
+            "11, XI",
+            "20, XX",
+            "50, L",
+            "73, LXXIII"
     })
     public void convert_arabic_to_roman(int arabic, String roman){
         RomanNumeralsConverter converter = new RomanNumeralsConverter();
